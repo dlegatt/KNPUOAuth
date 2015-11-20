4cdaecea8f6162a3a8392f66d2e726e06cb8ec47
David Legatt <legattd@ncbpinc.com>
2015-11-20 16:48:04 -0600

    initial commit

---
+++ cron/collect_eggs.php
@@ -0,0 +1,19 @@
+<?php
+
+include __DIR__.'/vendor/autoload.php';
+use Guzzle\Http\Client;
+
+// create our http client (Guzzle)
+$http = new Client('http://coop.apps.knpuniversity.com', array(
+    'request.options' => array(
+        'exceptions' => false,
+    )
+));
+
+
+$request = $http->post('/api/239/eggs-collect');
+$response = $request->send();
+
+echo $response->getBody();
+
+echo "\n\n";
